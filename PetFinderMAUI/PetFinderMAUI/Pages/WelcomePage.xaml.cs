4094e4109544edea1eb88824bcdc59e35bf3fe9c
MohammedChami <mohamuza@gmail.com>
2024-05-25 22:24:05 +0100

    feature: added there pages

---
+++ PetFinderMAUI/PetFinderMAUI/Pages/WelcomePage.xaml.cs
@@ -0,0 +1,18 @@
+namespace PetFinderMAUI.Pages;
+
+public partial class WelcomePage : ContentPage
+{
+	public WelcomePage()
+	{
+		InitializeComponent();
+	}
+	private async void btnLogin_Clicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new LoginPage());
+	}
+
+	private async void btnSignUp_Clicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new SignUpPage());
+	}
+}
