30453ac23f62ce1a492e306bee87f45a15b2637c
abdelillahbel <abdelillahbel@engineer.com>
2024-06-09 15:41:20 +0100

    app improvement ✨

--- PetFinderMAUI/PetFinderMAUI/MauiProgram.cs
+++ PetFinderMAUI/PetFinderMAUI/MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 using The49.Maui.BottomSheet;
 using UraniumUI;
 
@@ -10,14 +11,16 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder.UseMauiApp<App>().UseBottomSheet().UseUraniumUI().UseUraniumUIMaterial().ConfigureFonts(fonts =>
-        {
-            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            fonts.AddFont("Nunito-Bold.ttf", "NunitoBold");
-            fonts.AddFont("Nunito-Regular.ttf", "NunitoRegular");
-            fonts.AddMaterialIconFonts();
-        }).UseMauiCommunityToolkit();
+        builder.UseMauiApp<App>()
+            .UseSkiaSharp()
+            .UseBottomSheet().UseUraniumUI().UseUraniumUIMaterial().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Nunito-Bold.ttf", "NunitoBold");
+                fonts.AddFont("Nunito-Regular.ttf", "NunitoRegular");
+                fonts.AddMaterialIconFonts();
+            }).UseMauiCommunityToolkit();
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
