dd2ce563f8132fde55c77e1888607d48b7b09220
abdelillahbel <abdelillahbel@engineer.com>
2024-06-10 01:57:34 +0100

    app  improvement ✨

--- PetFinderMAUI/PetFinderMAUI/Entities/User.cs
+++ PetFinderMAUI/PetFinderMAUI/Entities/User.cs
@@ -4,8 +4,7 @@ public class User
 {
     public string? UserId { get; set; }
     public string? Username { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
+    public string? FullName { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Address { get; set; }
